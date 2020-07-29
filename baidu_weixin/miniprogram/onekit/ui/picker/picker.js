d1f28f2635bce52ba78ae17c8f86c4fd8bc0f6ac
gongyuan2 <939014677@qq.com>
2020-07-29 14:14:10 +0800

    更改bug

--- baidu_weixin/miniprogram/onekit/ui/picker/picker.js
+++ baidu_weixin/miniprogram/onekit/ui/picker/picker.js
@@ -6,33 +6,10 @@ Component({
   options: {  virtualHost: true
   },
   properties: {
-<<<<<<< HEAD
     Class:{type:String,value:""},
     Style:{type:String,value:""},
     value:{type:String,value:""},
     disabled:{type:Boolean,value:false},
-=======
-    Class: {
-      type: String,
-      value: ""
-    },
-    Style: {
-      type: String,
-      value: ""
-    },
-    Id: {
-      type: String,
-      value: ""
-    },
-    title:{type:String},
-    range:{type:Array},
-    rangeKey:{type:String},
-    value:{type:Number,value:0},
-    disabled:{type:Boolean,value:Boolean},
-    start:{type:String},
-    end:{type:String},
-    fields:{type:String},
->>>>>>> bfe817915d9d8e8e81548221f2cb69fc826d4c39
     mode:{type:String,value:"selector"},
     
   },
