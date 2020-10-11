d91a0df1b22dd8f63f8c9c42ef1c6dae1bd98cbe
zhangjinva <zhangjin_va@qq.com>
2020-10-11 12:29:41 +0800

    .

--- miniprogram_dev/baidu2weixin/OnekitComponent.js
+++ miniprogram_dev/baidu2weixin/OnekitComponent.js
@@ -82,12 +82,12 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 19);
+/******/ 	return __webpack_require__(__webpack_require__.s = 23);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 19:
+/***/ 23:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
