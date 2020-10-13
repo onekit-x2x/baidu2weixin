9b019118f36f62ca4dca05d03faff8eb2f5ed4db
lyx0lwz <69792963+lyx0lwz@users.noreply.github.com>
2020-10-13 12:19:42 +0800

    icon

--- miniprogram_dev/li/li.js
+++ miniprogram_dev/li/li.js
@@ -1,75 +1,56 @@
 import OnekitPage from '../baidu2weixin/OnekitPage';
 import swan from '../baidu2weixin/swan';
+var app = getApp();
 OnekitPage({
     data:{
-        appData:getApp().globalData.openParams,
-        isWeb:false,
-        isFavor:false
-    },
-    tapChange:function(){
-        this.setData({
-        isFavor:!this.data.isFavor
-    });
+        types:{
+            smallDefault:[
+                'success',
+                'info',
+                'warn',
+                'waiting',
+                'setting',
+                'top',
+                'search',
+                'personal',
+                'download',
+                'clear',
+                'close',
+                'cancel',
+                'success_no_circle',
+                'checkboxSelected',
+                'radioSelected',
+                'radioUnselect',
+                'loadingGrey'
+            ]
+        },
+        colors:[
+            '#3388FF',
+            '#F7534F',
+            '#FF6600',
+            '#000000'
+        ],
+        sizes:[
+            40,
+            34,
+            30,
+            24,
+            22,
+            18,
+            16
+        ]
     },
     onShow:function(){
-        const openParams = this.data.appData;
+        var openParams = app.globalData.openParams;
         if(openParams){
         swan.reportAnalytics('pageshow',{
             fr:openParams,
             type:'component',
-            name:'button'
+            name:'icon'
         });
     }
-        if((openParams === 'docWeb')){
-        this.setData('isWeb',true);
-    }
-        swan.getSystemInfo({
-        success:(res)=>{this.setData('isWeb',(res.platform === 'web'))}
-    });
     },
     onHide:function(){
-        this.data.appData = '';
-    },
-    submit:function(){
-        swan.showToast({
-        title:'用户点击了submit',
-        icon:'none'
-    });
-    },
-    reset:function(){
-        swan.showToast({
-        title:'用户点击了reset',
-        icon:'none'
-    });
-    },
-    tap:function(){
-        swan.showToast({
-        title:'已点击',
-        icon:'none'
-    });
-    },
-    getUserInfo:function(e){
-        console.log('用户信息:',e);
-        if(e.detail.encryptedData){
-        swan.showToast({
-            title:'已成功授权',
-            icon:'none'
-        });
-    }
-    },
-    openSetting:function(e){
-        console.log('用户设置:',e);
-    },
-    getPhoneNumber:function(e){
-        console.log('用户手机号:',e);
-    },
-    contact:function(e){
-        console.log('用户面板:',e);
-    },
-    chooseInvoiceTitle:function(e){
-        console.log('发票抬头:',e);
-    },
-    chooseAddress:function(e){
-        console.log('收货地址:',e);
+        app.globalData.openParams = '';
     }
 });
